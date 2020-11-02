fe9630945d7c25a91fd4918fdcee1c30a9b3912e
Mean <onkanya.w@20scoops.net>
2020-11-02 09:50:32 +0700

    add schema

--- packages/my-project/api/restaurant/config/schema.graphql.js
+++ packages/my-project/api/restaurant/config/schema.graphql.js
@@ -1,13 +1,34 @@
 module.exports = {
+  definition: `
+    type Person {
+      id: Int!
+      firstname: String!
+      lastname: String!
+      age: Int
+      children: [Person]
+    }
+  `,
+  query: `
+    person(id: Int!): Person
+  `,
+  type: {
+    Person: {
+      _description: "The Person type description", // Set the description for the type itself.
+      firstname: "The firstname of the person",
+      lastname: "The lastname of the person",
+      age: {
+        description: "The age of the person",
+        deprecated:
+          "We are not using the age anymore, we can find it thanks to our powerful AI",
+      },
+      children: "The children of the person",
+    },
+  },
   resolver: {
     Query: {
-      restaurants: {
-        description: "Return a list of restaurants", // Add a description to the query.
-      },
-    },
-    Mutation: {
-      createRestaurant: {
-        description: "Create a new restaurant",
+      person: {
+        _description: "Return a single person",
+        // resolver: "application::person.person.findOne", // It will use the action `findOne` located in the `Person.js` controller.
       },
     },
   },
